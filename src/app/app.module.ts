9989632643d792b552d82745680f554023279d51
Thangggg <thang.nguyenvan7594@gmail.com>
2022-03-09 11:49:46 +0700

    update giao dien pm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,6 +49,7 @@ export const appRoutes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'user-account', component: UserAccountComponent},
   {path: 'change-avatar', component: ChangeAvatarComponent},
+  {path: 'pm', component: PmComponent},
 
   {
     path: 'guide/getting-started',
