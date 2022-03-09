4c2a27d6d220b80f024f4fed5ff36f0a661615e2
Harrey Linn <tavantan100698@gmail.com>
2022-03-09 20:01:54 +0700

    update template login signup

--- src/app/form_login/login/login.component.ts
+++ src/app/form_login/login/login.component.ts
@@ -45,4 +45,5 @@ export class LoginComponent implements OnInit {
       }
     });
   }
+
 }
--- src/app/form_login/register/register.component.ts
+++ src/app/form_login/register/register.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 import {AuthService} from '../../service/auth.service';
 import {SignUpForm} from '../../model/SignUpForm';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -27,7 +28,7 @@ export class RegisterComponent implements OnInit {
   };
   status = 'Please fill in the form to Register!';
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -41,14 +42,20 @@ export class RegisterComponent implements OnInit {
       this.form.password
     );
     this.authService.signUp(this.signUpForm).subscribe(data => {
+      // tslint:disable-next-line:triple-equals
       if (JSON.stringify(data) == JSON.stringify(this.error1)) {
         this.status = 'The username is existed! Please try!';
       }
+      // tslint:disable-next-line:triple-equals
       if (JSON.stringify(data) == JSON.stringify(this.error2)) {
         this.status = 'The email is existed! Please try!';
       }
+      // tslint:disable-next-line:triple-equals
       if (JSON.stringify(data) == JSON.stringify(this.success)) {
         this.status = 'Create account success!';
+        this.router.navigate(['login']).then(() => {
+          window.location.reload();
+        });
       }
     });
   }
